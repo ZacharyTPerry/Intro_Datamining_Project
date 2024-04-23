95b7d053eb35d96181877321c21c868301a3d049
DaniallMasood <daniallmasood@gwu.edu>
2024-04-23 11:04:58 -0400

    updated files

--- daniall_models_graphs.py
+++ daniall_models_graphs.py
@@ -46,7 +46,7 @@ top_5_violations.plot(kind='barh', color='skyblue')
 plt.xlabel('Frequency')
 plt.ylabel('Violation Type')
 plt.title('Top 5 Traffic Violations')
-plt.gca().invert_yaxis()  # Invert y-axis to display the highest count at the top
+plt.gca().invert_yaxis()  
 plt.show()
 
 # %%
@@ -63,39 +63,11 @@ plt.bar(unique_tags.index, unique_tags.values, color='skyblue')
 plt.xlabel('State Tag')
 plt.ylabel('Count')
 plt.title('Distribution of Vehicle State Tags')
-plt.xticks(rotation=45)  # Rotate x-axis labels for better readability if needed
+plt.xticks(rotation=45)  
 plt.tight_layout()
 plt.show()
 
 
-# %%
-import pandas as pd
-import statsmodels.api as sm
-
-# Load the dataset
-#data = pd.read_csv('your_dataset.csv')  # Replace 'your_dataset.csv' with the actual filename
-
-# Creating dummy variables for 'PLATE_STATE'
-plate_state_dummies = pd.get_dummies(data['PLATE_STATE'], prefix='PLATE_STATE', drop_first=True)
-
-# Concatenate dummy variables with the original dataframe
-data = pd.concat([data, plate_state_dummies], axis=1)
-
-# Creating a binary column indicating whether the violation was paid or not
-data['VIOLATION_PAID'] = (data['DISPOSITION_TYPE'] == 'PAID').astype(int)
-
-# Define predictor variables including dummy variables
-predictors = ['FINE_AMOUNT', 'VIOLATION_CODE'] + list(plate_state_dummies.columns)
-
-# Add constant term for the intercept
-data['intercept'] = 1
-
-# Fit logistic regression model
-logit_model = sm.Logit(data['VIOLATION_PAID'], data[predictors + ['intercept']])
-result = logit_model.fit()
-
-# Print summary of the model
-print(result.summary())
 
 
 # %%
@@ -106,9 +78,7 @@ data.isnull().sum()
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Assuming 'ISSUING_AGENCY_CODE' is the column name for issuing agency code
-# Assuming 'FINE_AMOUNT' is the column name for fine amount
-# Assuming 'data' is your DataFrame
+
 
 # Calculate Pearson correlation coefficient
 correlation = data['ISSUING_AGENCY_CODE'].corr(data['FINE_AMOUNT'])
@@ -122,7 +92,7 @@ plt.xlabel('Issuing Agency Code')
 plt.ylabel('Fine Amount')
 plt.show()
 
-# Create a scatter plot (optional, for visualization)
+# Create a scatter plot 
 plt.figure(figsize=(10, 6))
 sns.scatterplot(x='ISSUING_AGENCY_CODE', y='FINE_AMOUNT', data=data)
 plt.title('Scatter Plot of Fine Amount by Issuing Agency')
@@ -134,8 +104,6 @@ plt.show()
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Assuming 'PLATE_STATE' is the column name for state and 'FINE_AMOUNT' is for fine amount
-# Assuming 'data' is your DataFrame
 
 # Calculate Pearson correlation coefficient
 correlation = data['PLATE_STATE'].corr(data['FINE_AMOUNT'])
@@ -147,7 +115,7 @@ sns.scatterplot(x='PLATE_STATE', y='FINE_AMOUNT', data=data)
 plt.title('Scatter Plot of Fine Amount by State')
 plt.xlabel('State')
 plt.ylabel('Fine Amount')
-plt.xticks(rotation=90)  # Rotate x-axis labels for better visibility
+plt.xticks(rotation=90) 
 plt.show()
 
 
@@ -155,7 +123,7 @@ plt.show()
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Assuming data is loaded into a DataFrame called df
+
 violation_counts = data['VIOLATION_CODE'].value_counts()
 violation_counts.plot(kind='bar')
 plt.title('Distribution of Violation Codes')
@@ -170,27 +138,6 @@ plt.xlabel('Fine Amount')
 plt.ylabel('Frequency')
 plt.show()
 
-# %%
-import pandas as pd
-import matplotlib.pyplot as plt
-
-# Assuming data is loaded into a DataFrame called data
-violation_process_counts = data['VIOLATION_PROCESS_DESC'].value_counts()
-
-# Select the top 5 categories
-top_5 = violation_process_counts.head(5)
-
-# Sum up counts of all other categories
-others_count = violation_process_counts.iloc[5:].sum()
-
-# Create a new Series with the top 5 categories and the 'Others' category
-modified_counts = top_5.append(pd.Series(others_count, index=['Others']))
-
-# Plot the pie chart
-plt.pie(modified_counts, labels=modified_counts.index, autopct='%1.1f%%')
-plt.title('Percentage of Top 5 Violation Process Descriptions')
-plt.axis('equal')
-plt.show()
 
 
 # %%
@@ -233,10 +180,10 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 # Load the data
-#data = pd.read_csv("your_data.csv")
+
 
 # Select relevant features and encode categorical variables
-# For example, let's assume 'PLATE_STATE' and 'VIOLATION_PROCESS_DESC' are relevant features
+#'PLATE_STATE' and 'VIOLATION_PROCESS_DESC' are relevant features
 # Encode 'PLATE_STATE' using LabelEncoder
 label_encoder = LabelEncoder()
 data['PLATE_STATE'] = label_encoder.fit_transform(data['PLATE_STATE'])
